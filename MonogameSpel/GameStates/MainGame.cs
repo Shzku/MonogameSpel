81574ebd4adb319d7aa4f00c8f83a6ddb21f4172
Seebus <19seth@stockholmscience.se>
2021-05-08 14:24:11 +0200

    Menu triggers Game

---
+++ MonogameSpel/GameStates/MainGame.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonogameSpel.GameStates
+{
+    public class MainGame : GameState
+    {
+        public MainGame(GraphicsDevice graphicsDevice) : base(graphicsDevice)
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+        }
+
+        public override void UnloadContent()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            //_graphicsDevice.Clear(Color.Aquamarine);
+            
+            //spriteBatch.Begin();
+            //spriteBatch.End();
+        }
+    }
+}
